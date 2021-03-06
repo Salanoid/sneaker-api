3755b1c80aa31ac7b1212a4d9a5d9d3e60adb679
Salanoid <salanoid@gmail.com>
2021-03-06 15:25:47 +0200

    initial commit of rails api app

---
+++ app/controllers/solecollector_scraper_controller.rb
@@ -0,0 +1,2 @@
+class SolecollectorScraperController < ApplicationController
+end
---
+++ test/controllers/solecollector_scraper_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SolecollectorScraperControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
